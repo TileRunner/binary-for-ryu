2823059bfb29a54c3031e8a7b01a3b8155150cff
TileRunner <justchrissykes@gmail.com>
2022-07-28 21:08:20 -0400

    Tidy up login UI
    Endless fiddling and trying xs="auto" which didn't seem to work as I wanted, settled on fixed width input boxes.
    Added validation to the fields.

--- fyb/src/login.js
+++ fyb/src/login.js
@@ -1,27 +1,50 @@
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
 const Login = ({setLoggedIn, username, setUsername, password, setPassword}) => {
-    function performValidation() {
-        return username.length > 0 && password.length > 0;
+    function isDataAcceptable() {
+        return username.length > 0 && password.length > 0 && isValidFormat(username) && isValidFormat(password);
     }
     function handleSubmit(event) {
         event.preventDefault();
         setLoggedIn(true);
     }
+    function isValidFormat(s) {
+        let alphanumericPattern = /^[ A-Za-z0-9]+$/;
+        return alphanumericPattern.test(s);
+    }
     return (
         <div className="Login">
-            <h1>Log in page</h1>
+            <h1>Login page</h1>
             <Form onSubmit={handleSubmit}>
-                <Form.Group controlId="username" className="mb-3">
-                    <Form.Label>Username:</Form.Label>
-                    <Form.Control autofocus type="text" value={username} onChange={e => setUsername(e.target.value)}></Form.Control>
+                <Form.Group as={Row} controlId="username" className="mb-3">
+                    <Form.Label column sm={1}>Username:</Form.Label>
+                    <Col sm={3}>
+                        <Form.Control
+                        className="mb-3"
+                        type="text"
+                        value={username}
+                        onChange={e => {setUsername(e.target.value);}}
+                        isInvalid={username && !isValidFormat(username)}
+                        />
+                        <Form.Control.Feedback type="invalid">must only use letters and/or numbers and/or spaces</Form.Control.Feedback>
+                    </Col>
                 </Form.Group>
-                <Form.Group controlId="password" className="mb-3">
-                    <Form.Label>Password:</Form.Label>
-                    <Form.Control autofocus value={password} onChange={e => setPassword(e.target.value)} type="password"></Form.Control>
+                <Form.Group as={Row} controlId="password" className="mb-3">
+                    <Form.Label column sm={1}>Password:</Form.Label>
+                    <Col sm={3}>
+                        <Form.Control
+                        className="mb-3"
+                        value={password}
+                        onChange={e => {setPassword(e.target.value);}} type="password"
+                        isInvalid={password && !isValidFormat(password)}
+                        />
+                        <Form.Control.Feedback type="invalid">must only use letters and/or numbers and/or spaces</Form.Control.Feedback>
+                    </Col>
                 </Form.Group>
-                <Button variant="primary" disabled={!performValidation()} type="submit">Log In</Button>
+                <Button variant="primary" disabled={!isDataAcceptable()} type="submit">Submit</Button>
             </Form>
         </div>
     );
