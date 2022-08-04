b5104497bb134a3b7385518e79b1546444be21a6
TileRunner <justchrissykes@gmail.com>
2022-08-04 10:50:00 -0400

    Make Survival playable - ish
    The game can be played but currently is not auto updating for actions from other players. There are no web sockets (the idea behind this rewrite) and my polling attempts did not go well ... react hooks confusion. So for now, players have to click a button to refresh data.

--- fyb/src/playSurvival.js
+++ fyb/src/playSurvival.js
@@ -3,7 +3,8 @@ import Button from "react-bootstrap/Button";
 import Table from 'react-bootstrap/Table';
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
-
+import Form from 'react-bootstrap/Form';
+import { isWordValid } from "./wordFunctions";
 const baseurl = 'https://enigmatic-lake-42795.herokuapp.com';
 
 const PlaySurvival = ({username}) => {
@@ -11,8 +12,9 @@ const PlaySurvival = ({username}) => {
     const [gamelist, setGamelist] = useState([]);
     const [gamedata, setGamedata] = useState({});
     const [inlobby, setInlobby] = useState(true);
+    const [myword, setMyword] = useState('');
 
-    async function getGamelist() {
+    async function refreshGamelist() {
         let url = `${baseurl}/fyb/listgames?type=SURVIVAL`;
         const response = await fetch(url);
         const jdata = await response.json();
@@ -20,11 +22,22 @@ const PlaySurvival = ({username}) => {
             setInfoMessage(jdata.error);
         } else {
             setGamelist(jdata);
-            setInfoMessage('List loaded');
+            setInfoMessage(`List loaded at ${formatTime(Date.now())}`);
+        }
+    }
+    async function refreshGamedata() {
+        let url = `${baseurl}/fyb/getgame?number=${gamedata.number}`;
+        const response = await fetch(url);
+        const jdata = await response.json();
+        if (jdata.error) {
+            setInfoMessage(jdata.error);
+        } else {
+            setGamedata(jdata);
+            setInfoMessage(`Game loaded at ${formatTime(Date.now())}`);
         }
     }
     useEffect(() => {
-        getGamelist();
+        refreshGamelist();
     },[]);
     async function createNewGame() {
         let url = `${baseurl}/fyb/creategame?type=SURVIVAL&name=${username}`;
@@ -50,24 +63,39 @@ const PlaySurvival = ({username}) => {
             setInfoMessage('Game joined');
         }
     }
+    async function startGame() {
+        let url = `${baseurl}/fyb/startgame?number=${gamedata.number}`;
+        const response = await fetch(url);
+        const jdata = await response.json();
+        if (jdata.error) {
+            setInfoMessage(jdata.error);
+        } else {
+            setGamedata(jdata);
+            setInfoMessage('Game started');
+        }
+    }
     const formatTime = (t) => {
         let d = new Date(t);
+        let yyyy = d.getFullYear().toString();
+        let mth = (d.getMonth() + 1).toString();
+        if (mth.length < 2) { mth = '0' + mth; }
+        let dd = d.getDate().toString();
+        if (dd.length < 2) { dd = '0' + dd; }
         let h = d.getHours();
         let ampm = ' AM';
         if (h > 12) {
             h = h - 12;
             ampm = ' PM';
         }
+        let hh = h.toString();
+        if (h < 10) { hh = '0' + hh; }
         let m = d.getMinutes();
-        let mm = (d.getMonth() + 1).toString();
-        if (mm.length < 2) { mm = '0' + mm;}
-        let dd = d.getDate().toString();
-        if (dd.length < 2) { dd = '0' + dd;}
-        let f = d.getFullYear().toString() + '-' + mm + '-' + dd + ' @';
-        f = f + h.toString() + ':';
-        if (m < 10) { f = f + '0';}
-        f = f + m.toString();
-        f = f + ampm;
+        let mm = m.toString();
+        if (m < 10) { mm = '0' + mm; }
+        let s = d.getSeconds();
+        let ss = s.toString();
+        if (s < 10) { ss = '0' + ss; }
+        let f = `${yyyy}-${mth}-${dd} @${hh}:${mm}:${ss} ${ampm}`;
         return f;
     }
     const ShowGameList = <div>
@@ -91,46 +119,110 @@ const PlaySurvival = ({username}) => {
                     <td>{formatTime(game.createTime)}</td>
                     <td>{game.numPlayers}</td>
                     <td>{game.finished ? 'Finished' : game.started ? 'In Progress' : 'Not Started'}</td>
-                    <td>{game.started ? <span>No action</span>
-                    : <Button key={`joinbutton${game.number}`}
-                    onClick={() => {joinGame(game.number);}}>
-                        Join
-                    </Button>
-                    }</td>
+                    <td>
+                        <Button key={`joinbutton${game.number}`}
+                        onClick={() => {joinGame(game.number);}}>
+                            Join
+                        </Button>
+                    </td>
                 </tr>
             ))}
             </tbody>
         </Table>
+        <Button onClick={() => {refreshGamelist();}}>
+            Refresh Game List
+        </Button>
         <Button onClick={() => {createNewGame();}}>
             Create Game
         </Button>
-        <p>Game: {JSON.stringify(gamedata)}</p>
-        <p>Gamelist: {JSON.stringify(gamelist)}</p>
     </div>
+    function meToMove() {
+        if (!gamedata.started) {return false;}
+        for (let index = 0; index < gamedata.players.length; index++) {
+            const player = gamedata.players[index];
+            if (player.name === username) {
+                return player.tomove;
+            }
+        }
+    }
+    async function handleSubmit(event) {
+        event.preventDefault();
+        let url = `${baseurl}/fyb/makemove?number=${gamedata.number}&name=${username}`;
+        if (myword) {
+            let valid = await isWordValid(myword);
+            if (valid) {
+                url = `${url}&type=VALID&word=${myword}`;
+            } else {
+                url = `${url}&type=PHONY&word=${myword}`;
+            }   
+        } else {
+            url = `${url}&type=PASS`
+        }
+        const response = await fetch(url);
+        const jdata = await response.json();
+        if (jdata.error) {
+            setInfoMessage(jdata.error);
+        } else {
+            setGamedata(jdata);
+            setInfoMessage(`${username} made a move`);
+        }
+    }
+    function isValidFormat(s) {
+        let alphabeticPattern = /^[A-Za-z]+$/;
+        return alphabeticPattern.test(s);
+    }
     const ShowGame = <div>
         <Row>
-            <Col>Game Number:</Col>
-            <Col>{gamedata.number}</Col>
+            <Col>Game Number: {gamedata.number}</Col>
+            <Col>Creator: {gamedata.creator}</Col>
+            <Col>Created: {formatTime(gamedata.createTime)}</Col>
         </Row>
+        {!gamedata.started &&
+        <Button onClick={() => {startGame();}}>
+            Start Game
+        </Button>
+        }
+        {gamedata.started &&
         <Row>
-            <Col>Chat Number:</Col>
-            <Col>{gamedata.chatNumber}</Col>
+            <Col>Round: {gamedata.round}</Col>
+            <Col>Fry Letters: {gamedata.letters.slice(0,2+gamedata.round).join("").toUpperCase()}</Col>
+            {meToMove() && <Col>
+            <Form onSubmit={handleSubmit}>
+                <Form.Label>Your Word:</Form.Label>
+                <Form.Control
+                type="text"
+                value={myword}
+                onChange={e => { setMyword(e.target.value); } }
+                isInvalid={myword && !isValidFormat(myword)}
+                />
+                <Form.Control.Feedback type="invalid">Must only use letters</Form.Control.Feedback>
+            </Form>
+            </Col>
+            }
         </Row>
+        }
         <Row>
-            <Col>Creator:</Col>
-            <Col>{gamedata.creator}</Col>
+            {gamedata.players && gamedata.players.map((player,index) => (
+                <Col key={`player${player.name}`}>
+                    {player.name}
+                    {gamedata.started ?
+                        player.alive ?
+                            player.tomove ?
+                                <span> To move...</span>
+                            :
+                                <span> Survived!</span>
+                        :
+                            <span> Eliminated!</span>
+                    :
+                        <span>...</span>
+                    }
+                </Col>
+            ))}
         </Row>
-        <Row>
-            <Col>Created:</Col>
-            <Col>{formatTime(gamedata.createTime)}</Col>
-        </Row>
+        <Button onClick={() => {refreshGamedata();}}>
+            Refresh Game Data
+        </Button>
     </div>
-    useEffect(() => {
-        const id = setInterval(() => {
-            getGamelist();
-        }, 5000);
-        return () => clearInterval(id);
-    }, []);
     return <div className="PlaySurvival">
         <h1>Survival Game</h1>
         <p>Info: {infoMsg}</p>
